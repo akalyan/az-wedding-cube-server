273434309260c1d8c11035ee77eee387b25f0766
Atish Kalyan <akalyan.dev@gmail.com>
2013-10-11 23:07:36 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var c_options = require("./collector-config"),
+    e_options = require("./evaluator-config"),
+    cube = require("../"),
+    c_server = cube.server(c_options),
+    e_server = cube.server(e_options);
+
+c_server.register = function(db, endpoints) {
+  cube.collector.register(db, endpoints);
+};
+
+e_server.register = function(db, endpoints) {
+  cube.evaluator.register(db, endpoints);
+};
+
+c_server.start();
+e_server.start();
