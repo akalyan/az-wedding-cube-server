7d46d754e20579517dfcd837604b39be7939d7d4
Atish Kalyan <akalyan.dev@gmail.com>
2013-10-13 17:25:55 -0700

    Trying to run cube collector and evaluator on port 80

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-var c_options = require("./collector-config"),
-    e_options = require("./evaluator-config"),
+var options = require("./collector-config"),
     cube = require("cube"),
-    c_server = cube.server(c_options),
-    e_server = cube.server(e_options);
+    server = cube.server(options);
 
-c_server.register = function(db, endpoints) {
+server.register = function(db, endpoints) {
   cube.collector.register(db, endpoints);
-};
-
-e_server.register = function(db, endpoints) {
   cube.evaluator.register(db, endpoints);
 };
 
-c_server.start();
-e_server.start();
+server.start();
